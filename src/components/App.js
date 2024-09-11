f778d8dd1a3803fae3a336bfff57b638d33af532
Th3c0d3d1 <bittradeguy007@gmail.com>
2024-09-11 12:55:54 -0400

    Loading accounts to MMK

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,45 @@
+import { useEffect, useState } from "react"
 import { Container } from "react-bootstrap"
+import { ethers } from "ethers"
 
 // Components
-import Nav from "./Nav.js"
+import Navigation from "./Nav.js"
+import Info from "./Info.js"
+
 
 function App() {
+    // account -> Variable of current account value
+    // setAccount('0x0...') -> Function to update account value
+    // null -> Default value
+    const [account, setAccount] = useState(null)
+
+    // window.ethereum is fetched to wire to app
+    const loadBlockchainData = async () => {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        
+        // Set the MMK accounts to the accounts variable
+        const accounts = await window.ethereum.request({method: 'eth_requestAccounts'})
+        const account = await ethers.utils.getAddress(accounts[0])
+
+        // Add account to state
+        setAccount(account)
+    }
+
+    
+
+    useEffect(() => {
+        loadBlockchainData()
+    });
+
     return(
         <Container>
-            <Nav />
+            <Navigation />
+            <hr />
+            {/* Checks for availability of accounts */}
+            {account && (
+                <Info account={account}/>
+            )}
+            {/* Read account from state */}
         </Container>
     )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,18 +2,19 @@ import Navbar from "react-bootstrap/Navbar"
 
 import logo from "../logo.png"
 
-const Nav = () => {
+const Navigation = () => {
     return(
         <Navbar>
-            <img alt="logo"
-            src={logo}
-            width="40"
-            height="40"
-            className="d-inline-block align-top mx-3"
+            <img 
+                alt="logo"
+                src={logo}
+                width="40"
+                height="40"
+                className="d-inline-block align-top mx-3"
             />
-            <Navbar.Brand href="#">Next Gen ICO Crowdsale</Navbar.Brand>
+            <Navbar.Brand href="#">Next Gen ICO Crowdsale!</Navbar.Brand>
         </Navbar>
     )
 }
 
-export default Nav;
+export default Navigation;
